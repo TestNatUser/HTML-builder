fee9e7e2ea7d59e35a68d7b2f854c034050cbc39
Natalia Furmanova <nfurmanova@tula.co>
2025-01-19 23:23:37 +0300

    feat: 04-copy-directory

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const fs = require('fs')
+
+fs.mkdir(path.join(__dirname,'file-copy'),(err)=>{
+  if(err) process.exit();
+});
+fs.readdir(path.join(__dirname,'files'),(err,files)=>{
+  if(err) process.exit();
+  files.forEach(file=>{
+    fs.copyFile(path.join(__dirname,`files/${file}`),path.join(__dirname,`file-copy/${file}`),(err)=>{
+      if(err) process.exit();
+    })
+  })
+})

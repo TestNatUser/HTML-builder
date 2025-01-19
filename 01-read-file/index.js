13246508d50099d468cb199bf9472f638def1b30
Natalia Furmanova <nfurmanova@tula.co>
2025-01-19 19:43:56 +0300

    fix: add check for read file task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 
 var stream = new fs.ReadStream(path.join(__dirname,'text.txt'),{encoding: 'utf-8'});
 
-stream.on('readable', function(){
-    var data = stream.read();
+stream.on('readable', () => {
+  let data;
+  while ((data = stream.read()) !== null) {
     console.log(data);
+  }
 });
